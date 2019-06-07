d0b2ab8d48131f13e16a03baad26b5198302cacd
unknown <Flamur Deliu>
2019-06-07 22:46:12 +0200

    Updates

---
+++ precache-manifest.724f8b818f20dda001f506870cca66ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f3c3d89b7e7d5349acb9576d99f4e6",
+    "url": "/image-finder/index.html"
+  },
+  {
+    "revision": "f3f6e92364997f97fc39",
+    "url": "/image-finder/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "7f39a6440e27e8bac571",
+    "url": "/image-finder/static/js/2.aa0f2688.chunk.js"
+  },
+  {
+    "revision": "f3f6e92364997f97fc39",
+    "url": "/image-finder/static/js/main.05511af5.chunk.js"
+  },
+  {
+    "revision": "f192cf7c5a241cf129dd",
+    "url": "/image-finder/static/js/runtime~main.26a19609.js"
+  }
+]);
